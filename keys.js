4a9c6f21f6e2ddd3e8b43e522b0181a8bc72c04a
Tommy Oliver <Tommy Oliver>
2017-10-01 18:28:41 -0600

    Commit

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'fe67CYTQQajCL3WxAe0VUy9V0',
+  consumer_secret: '3ASsKV1zKpnCOkm13ESVkmq4zWpi5Z07aMp1Ha8UnjC8xXCRwT',
+  access_token_key: '910706943626547200-p7GM2oV3poxZIkxmUa4xireKEXSOcBO',
+  access_token_secret: 'i54JiNvs4whD4RxUP3knTgwNblLRQ3ShgNymGsi94sDIk',
+}
+
+var spotifyKeys = {
+	id: 'f21fec2ac9564382b24dcedab2f2fe05',
+	secret: 'f2adf51de4374330bc483b852402c13f'
+}
+module.exports = {
+	twitterKeys:twitterKeys,
+	spotifyKeys:spotifyKeys
+}
---
+++ liri.js
@@ -0,0 +1,153 @@
+var fs = require('fs');
+var request = require('request');
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+
+    
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+    })
+
+    var spotify = new Spotify({
+      id: keys.spotifyKeys.id,
+      secret: keys.spotifyKeys.secret
+    });
+
+var operator=process.argv[2];
+
+switch(operator) {
+    case("my-tweets"): {
+        myTweets();
+        return;
+    }
+    case("spotify-this-song"): {
+        selectSong();
+        return;
+    }
+    case("movie-this"): {
+        selectMovie();      
+        return;
+
+    }
+    case("do-what-it-says"): {
+        fs.readFile('random.txt', 'utf8', function(err, data) {
+            if(err) {
+                return console.log(err);
+                }
+                var directions = data.split(", ");  
+                console.log(directions[0]);
+                console.log(directions[1]);
+                
+                switch(directions[0]) {
+                    case "my-tweets": {
+                        myTweets();
+                        return;
+                    }
+                    case "spotify-this-song": {
+                        selectSong(directions[1]);
+                        return
+                    }
+                    case "movie-this": {
+                        selectMovie(directions[1]);
+                        return
+                    }
+                    default: {
+                        console.log("Nothing in file");
+                        return;
+                    }
+                }
+            });
+        return;
+    }
+
+   
+}
+
+function myTweets() {
+    var params = {screen_name: "TheFakeTO2", count: 20};
+
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+        if (!error) {
+          for (var i = 0; i < tweets.length; i++) {
+            console.log("Created: ${tweets[i].created_at}\nTweet: ${tweets[i].text}\n");
+          }
+        }
+    })
+}
+
+function selectSong(songName) {
+    var songName = process.argv[3];
+        var limit = 10;
+
+        if(!songName){
+            spotify.search({ type: "track", query: "The Sign Ace of Base", limit: limit}, function(err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                
+
+                    // console.log(data.tracks.items[2].album);
+                for(var i = 0; i < limit; i++) {
+
+                    console.log("Result ${i+1}");
+                    console.log("Artist(s) Name: ${data.tracks.items[i].artists[0].name}"); 
+                    console.log("Album Name: ${data.tracks.items[i].album.name}"); 
+                    console.log("Song Name: ${data.tracks.items[i].name}");  
+                    console.log("Spotify Preview Link: ${data.tracks.items[i].external_urls.spotify}"); 
+                    console.log("Popularity: ${data.tracks.items[i].popularity}"); 
+                    console.log("\n");
+                }
+            });
+        } else {
+            spotify.search({ type: 'track', query: songName, limit: limit}, function(err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+              }
+                    console.log(`----------------------------------------`);
+                    console.log(`Search Results for Song: ${songName}`);
+                    console.log(`----------------------------------------`);
+                    console.log(`\n`);
+
+                for(var i = 0; i < limit; i++) {
+
+                    console.log(`Result ${i+1}`);
+                    console.log(`----------------------------------------`);
+                    console.log(`Artist(s) Name: ${data.tracks.items[i].artists[0].name}`); 
+                    console.log(`Album Name: ${data.tracks.items[i].album.name}`); 
+                    console.log(`Song Name: ${data.tracks.items[i].name}`);  
+                    console.log(`Spotify Preview Link: ${data.tracks.items[i].external_urls.spotify}`); 
+                    console.log(`Popularity: ${data.tracks.items[i].popularity}`); 
+                    console.log(`----------------------------------------`);
+                    console.log(`\n`);
+                }
+            });
+        }
+}
+
+function selectMovie(movieName) {
+    // var movieName = process.argv[3];
+        // Then run a request to the OMDB API with the movie specified
+        if (process.argv[3]) {
+            var queryUrl = "http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=40e9cece";
+          } else {
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+          }
+        // console.log(queryUrl);
+        request(queryUrl, function(error, response, body) {
+          // If the request is successful (i.e. if the response status code is 200)
+              if (!error && response.statusCode === 200) {
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Produced: " + JSON.parse(body).Country);
+                console.log("Langauge: " + JSON.parse(body).Language);
+                console.log("Plot: " +  JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+              }
+        });
+}
